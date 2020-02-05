960412856a21a0874fe7b6e952036ae5ff85da63
uni143 <43483711+uni143@users.noreply.github.com>
2020-02-05 09:37:18 -0500

    Fixed pause/casting bug and changed version 2019.2.18f1

--- Spell Test/Assets/Spell.cs
+++ Spell Test/Assets/Spell.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.Windows.Speech;
-using TMPro;
-using Random = System.Random;
 
 /// -----------
 /// CISC 496 - Group P1 - Project: Eye Say
@@ -16,42 +11,10 @@ using Random = System.Random;
 
 public class Spell : MonoBehaviour
 {
-    static Random rnd = new Random();
-    public GameObject pcCanvas;
-    public TextMeshProUGUI textMeshM;
     public GameObject projectile;
     public float speed = 100f;
+    public GameObject ui;
 
-    public List<String> subjects = new List<string>();
-    public List<String> predicates = new List<string>();
-    public List<String> objects = new List<string>();
-
-    private KeywordRecognizer keywordRecognizer;
-    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
-
-    void Start()
-    {
-        pcCanvas = GameObject.Find("pcCanvas");
-        textMeshM = pcCanvas.transform.Find("spells").GetComponent<TextMeshProUGUI>();
-
-        subjects.Add("You ");
-        subjects.Add("I ");
-        predicates.Add("eat ");
-        predicates.Add("drink ");
-        objects.Add("food");
-        objects.Add("water");
-
-        GenerateSpell();
-
-        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
-        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
-        keywordRecognizer.Start();
-    }
-    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
-    {
-        Debug.Log(speech.text);
-        actions[speech.text].Invoke();
-    }
     /// ---
     /// Fire ball spell, make sure projectile spell has gravity set to 0
     /// Shoots fireball in forward direction of object it is attached to
@@ -60,27 +23,16 @@ public class Spell : MonoBehaviour
     /// ---
     void Fireball()
     {
-
-        GameObject fireBall = Instantiate(projectile, transform.position + Camera.main.transform.forward * 2, Quaternion.identity) as GameObject;
-        Rigidbody fireBallRigidBody = fireBall.GetComponent<Rigidbody>();
-        fireBallRigidBody.AddForce(Camera.main.transform.forward * speed);
-        GenerateSpell();
+        if (!(ui.GetComponent<GameMenu>().isPaused))
+        {
+            GameObject fireBall = Instantiate(projectile, transform.position + transform.forward * 2, Quaternion.identity) as GameObject;
+            Rigidbody fireBallRigidBody = fireBall.GetComponent<Rigidbody>();
+            fireBallRigidBody.AddForce(Vector3.forward * speed);
+        }
     }
 
-    public void GenerateSpell()
-    {
-        int r1 = rnd.Next(subjects.Count);
-        int r2 = rnd.Next(predicates.Count);
-        int r3 = rnd.Next(objects.Count);
-
-        string spell = subjects[r1] + predicates[r2] + objects[r3];
-        textMeshM.text = spell;
-        Debug.Log(spell);
-        actions.Add(spell, Fireball);
-    }
     /// Temporary trigger so that I could test the spell without a mic,
     ///     DELETE in final build, or keep as secret debug-mode function
-/*
     void Update()
     {
         //Placeholder trigger, replace with successful incantation
@@ -89,5 +41,4 @@ public class Spell : MonoBehaviour
             Fireball();
         }
     }
-*/
 }
