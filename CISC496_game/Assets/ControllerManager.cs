6511397e5bd23aa2c72e716f6e98adac639b96fa
HansinNA <z416102744@gmail.com>
2020-02-20 02:22:21 -0500

    Level1 Redo

---
+++ CISC496_game/Assets/ControllerManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+
+public class ControllerManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (SteamVR_Actions.default_GrabPinch.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            gameObject.GetComponent<GenerateSpell>().EnableRecognizer();
+        }
+        if (SteamVR_Actions.default_GrabPinch.GetStateUp(SteamVR_Input_Sources.Any))
+        {
+            gameObject.GetComponent<GenerateSpell>().DisableRecognizer();
+        }
+    }
+}
--- CISC496_game/Assets/basicController.cs
+++ CISC496_game/Assets/basicController.cs
@@ -2,31 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// -- DO NOT USE --
-/// CISC 496 - Group P1 - Project: Eye Say
-/// Description: DO NOT USE, a basic controller thrown together so I test moving the player to the goal
-/// Written by: Sammy Chan
-/// ---------- 
-
-public class basicController : MonoBehaviour
+public class BasicController : MonoBehaviour
 {
-    Rigidbody rb;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+    public float speed = 0.5f;
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
-            rb.AddForce(Vector3.left);
-        if (Input.GetKey(KeyCode.D))
-            rb.AddForce(Vector3.right);
-        if (Input.GetKey(KeyCode.W))
-            rb.AddForce(Vector3.forward);
-        if (Input.GetKey(KeyCode.S))
-            rb.AddForce(Vector3.back);
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.z -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
     }
 }
